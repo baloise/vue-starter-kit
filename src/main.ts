d3fefd09725e6a28419630aeacdb04a4be75041e
hirsch <gery.hirschfeld@w3tec.ch>
2021-03-25 12:42:52 +0100

    fix ui library

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import './styles/main.scss'
 
 import { createApp } from 'vue'
-import { baloiseUiLibrary } from '@baloise/ui-library-vue'
 
+import { baloiseUiLibrary } from '@baloise/ui-library-vue'
 import { router } from './plugins/router.plugin'
+import { http } from './plugins/http.plugin'
 import { i18n } from './plugins/i18n.plugin'
 
 import App from './app/App.vue'
 
-createApp(App).use(router).use(i18n).use(baloiseUiLibrary).mount('#app')
+createApp(App)
+  .use(router)
+  .use(http)
+  .use(i18n)
+  .use(baloiseUiLibrary, {
+    useVite: true,
+  })
+  .mount('#app')
