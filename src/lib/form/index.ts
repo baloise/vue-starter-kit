f36345875495d48e9e5bbe8a41ad9d9baf80d81e
hirsch <gery.hirschfeld@w3tec.ch>
2021-03-25 13:06:50 +0100

    clean

--- src/lib/form/index.ts
+++
@@ -1,3 +0,0 @@
-export { useForm } from './form'
-export { useFormControl } from './form-control'
-export { useControl } from './control'
--- tests/e2e/support/pages/home-page.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonAccessor, dataTestSelector } from '@baloise/ui-library-testing'
-
-export class HomePage {
-  button = ButtonAccessor(dataTestSelector('button-count'))
-
-  open() {
-    cy.visit('/')
-  }
-}
