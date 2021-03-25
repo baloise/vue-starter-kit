761b25d037789e3873ed1fee5049e9b973bb288e
hirsch <gery.hirschfeld@w3tec.ch>
2021-03-25 12:49:17 +0100

    increase warn limit for bundles

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,9 @@ module.exports = {
   configureWebpack: {
     mode: VUE_APP_MODE,
   },
+  chainWebpack: (config) => {
+    config.performance.maxEntrypointSize(1000000).maxAssetSize(1000000)
+  },
   publicPath: VUE_APP_MODE === 'production' ? '/app-name' : '/',
   devServer:
     VUE_APP_MODE !== 'production'
