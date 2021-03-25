a996f5ba4c6c23f05e528368c158381990ff063b
hirsch <gery.hirschfeld@w3tec.ch>
2021-03-25 12:45:37 +0100

    add form

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,11 +8,14 @@ export const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
+    path: '/form',
+    name: 'Form',
+    component: () =>
+      import(/* webpackChunkName: "form" */ '../app/pages/Form.vue'),
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../app/pages/About.vue'),
   },
