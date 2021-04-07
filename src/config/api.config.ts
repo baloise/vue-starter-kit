746e5a69c5fd6898d991cbc5b2d698b31de80403
hirsch <gery.hirschfeld@w3tec.ch>
2021-04-07 12:37:50 +0200

    update api config

--- src/config/api.config.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosRequestConfig } from 'axios'
-
-export const defaultApiConfig: AxiosRequestConfig = {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-}
--- src/config/app.config.ts
+++
@@ -1,3 +0,0 @@
-export const appConfig = {
-  apiPath: import.meta.env.API_PATH as string,
-}
